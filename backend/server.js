adba5fd5564f192eb19f2d8827eaa84b2b576799
Rohit Kumar <rohitkv090@gmail.com>
2023-02-21 22:47:02 +0530

    fixed errors

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use("/api/restaurant", restaurantRouter);
 app.use("/api/search", searchRouter);
 app.use("/api/users/", userRouter);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
